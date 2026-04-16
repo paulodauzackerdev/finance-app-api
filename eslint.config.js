f893be6778cc10d43179e32a749a21321cf77f19
Paulo Dauzacker <paulo_dauzacker@outlook.com.br>
2026-04-16 03:49:36 -0400

    chore: setup eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import prettier from 'eslint-config-prettier';
+
+export default defineConfig([
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.node
+    }
+  },
+  js.configs.recommended,
+  prettier,
+  {
+    rules: {
+      'no-console': 'off'
+    }
+  }
+]);
