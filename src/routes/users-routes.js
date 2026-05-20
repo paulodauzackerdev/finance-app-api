6422ea0a5fe5370d57fbdcfc9040daa90886d24c
Paulo Dauzacker <paulo_dauzacker@outlook.com.br>
2026-05-20 01:16:07 -0300

    feat: delete route added

--- src/routes/users-routes.js
+++ src/routes/users-routes.js
@@ -4,6 +4,7 @@ import { GetUserByIdController } from '../controllers/get-user-by-id.js'
 import { GetUserByEmailController } from '../controllers/get-user-by-email.js'
 import { UpdateUserController } from '../controllers/update-user.js'
 import { GetAllUsersController } from '../controllers/get-all-users.js'
+import { DeleteUserController } from '../controllers/delete-user.js'
 
 const usersRoutes = Router()
 
@@ -12,6 +13,7 @@ const getUserByIdController = new GetUserByIdController()
 const getUserByEmailController = new GetUserByEmailController()
 const updateUserController = new UpdateUserController()
 const getAllUsersController = new GetAllUsersController()
+const deleteUserController = new DeleteUserController()
 
 usersRoutes.get('/', (req, res) => getAllUsersController.handle(req, res))
 
@@ -23,5 +25,7 @@ usersRoutes.get('/email/:email', (req, res) =>
 usersRoutes.get('/:id', (req, res) => getUserByIdController.handle(req, res))
 
 usersRoutes.patch('/:id', (req, res) => updateUserController.handle(req, res))
+
+usersRoutes.delete('/:id', (req, res) => deleteUserController.handle(req, res))
 
 export { usersRoutes }
