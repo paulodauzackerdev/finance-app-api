30b73249d3b20f63712efc0120da9358964d35ba
Paulo Dauzacker <paulo_dauzacker@outlook.com.br>
2026-06-19 17:00:05 -0300

    refactor: remove manual validators replaced by Zod schemas

--- src/validators/transaction/validate-transaction-id.js
+++
@@ -1,9 +0,0 @@
-import validator from 'validator'
-import { InvalidTransactionIdError } from '../../errors/transaction.js'
-
-export const validateTransactionId = (transactionId) => {
-  if (!validator.isUUID(transactionId)) {
-    throw new InvalidTransactionIdError()
-  }
-  return transactionId
-}
--- src/validators/user/index.js
+++
@@ -1,4 +0,0 @@
-export { validateEmail } from './validate-email.js'
-export { validateName } from './validate-name.js'
-export { validatePassword } from './validate-password.js'
-export { validateUserId } from './validate-user-id.js'
