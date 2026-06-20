bfc599de1edb0ca5799e3e3caa4d59b0164d96bf
Paulo Dauzacker <paulo_dauzacker@outlook.com.br>
2026-06-20 16:43:33 -0300

    feat(security): increase bcrypt salt rounds to 12

--- src/helpers/password.js
+++ src/helpers/password.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt'
 
 export const hashPassword = async (password) => {
-  return bcrypt.hash(password, 10)
+  return bcrypt.hash(password, 12)
 }
 
 export const comparePassword = async (password, passwordHash) => {
