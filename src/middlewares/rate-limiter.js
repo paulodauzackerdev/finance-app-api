f826bb9b04639e46b1413fc11697d9a0be18547a
Paulo Dauzacker <paulo_dauzacker@outlook.com.br>
2026-07-02 22:07:16 -0300

    feat: add rate limiters for login, refresh, and user creation

--- src/middlewares/rate-limiter.js
+++ src/middlewares/rate-limiter.js
@@ -9,3 +9,33 @@ export const globalLimiter = rateLimit({
     error: 'Too many requests, please try again later'
   }
 })
+
+export const loginLimiter = rateLimit({
+  windowsMs: 18 * 60 * 1000,
+  max: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    error: 'Too many login attempts, please try again later'
+  }
+})
+
+export const refreshLimiter = rateLimit({
+  windowsMs: 15 * 60 * 1000,
+  max: 10,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    error: 'Too many refresh attempts, please try again later'
+  }
+})
+
+export const createUserLimiter = rateLimit({
+  windowsMs: 60 * 60 * 1000,
+  max: 3,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    error: 'Too many accounts created from this IP, please try again later'
+  }
+})
