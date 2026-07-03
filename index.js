d2ff75003e37031084fd86942c82a970b141d978
Paulo Dauzacker <paulo_dauzacker@outlook.com.br>
2026-07-02 22:07:21 -0300

    feat: register auth routes in express app

--- index.js
+++ index.js
@@ -7,20 +7,19 @@ import { corsMiddleware } from './src/middlewares/cors.js'
 import { errorHandler } from './src/middlewares/error-handler.js'
 
 import { docsRoutes } from './src/routes/docs-routes.js'
+import { authRoutes } from './src/routes/auth-routes.js'
 import { usersRoutes } from './src/routes/users-routes.js'
 import { transactionsRoutes } from './src/routes/transactions-routes.js'
 
 const app = express()
 
 app.use(helmet())
-
 app.use(corsMiddleware)
-
 app.use(globalLimiter)
-
 app.use(express.json())
 
 app.use('/docs', docsRoutes)
+app.use('/api/auth', authRoutes)
 app.use('/api/users', usersRoutes)
 app.use('/api/transactions', transactionsRoutes)
 
